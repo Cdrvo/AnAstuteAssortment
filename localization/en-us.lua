719b0cedae0fa51f365d7e9cfca66fe93d04dd6c
Alvin Dong <alvindong9913@gmail.com>
2025-08-04 20:47:09 -0500

    Add Crystals, set up localization
    Crystals is still bugged

---
+++ localization/en-us.lua
@@ -0,0 +1,26 @@
+return {
+    descriptions = {
+        Spectral = {
+            c_asa_crystals = {
+                name = "Crystals",
+                text = {
+                    "All {C:attention}Glass Cards{}",
+                    "destroyed this",
+                    "run are added back",
+                    "to your deck"
+                }
+            },
+        },
+        Joker = {
+            j_asa_scrambled_eggs = {
+                name = "Scrambled Eggs",
+                text = {
+                    "Dummy text"
+                }
+            }
+        }
+    },
+    misc = {
+
+    }
+}
--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
-SMODS.Atlas {
+SMODS.Atlas { -- spectral atlas
     key = "asa_spectrals",
     px = 71,
     py = 95,
     path = "spectrals.png"
 }
-SMODS.Atlas {
+SMODS.Atlas { -- joker atlas
     key = "asa_jokers",
     px = 71,
     py = 95,
