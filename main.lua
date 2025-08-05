84eb398703cd25d287316b064e10780cad418c7c
Alvin Dong <alvindong9913@gmail.com>
2025-08-04 19:31:36 -0500

    Add atlases
    Define the atlases for the mod

---
+++ main.lua
@@ -0,0 +1,12 @@
+SMODS.Atlas {
+    key = "asa_spectrals",
+    px = 71,
+    py = 95,
+    path = "spectrals.png"
+}
+SMODS.Atlas {
+    key = "asa_jokers",
+    px = 71,
+    py = 95,
+    path = "jokers.png"
+}
