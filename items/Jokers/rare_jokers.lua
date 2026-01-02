ddd48fc62abf2eff0e159c6b4276491385b17faa
CodeRevo <107761434+Cdrvo@users.noreply.github.com>
2026-01-02 22:35:31 +0300

    Check buff

--- items/Jokers/rare_jokers.lua
+++ items/Jokers/rare_jokers.lua
@@ -66,7 +66,7 @@ SMODS.Joker({
 	blueprint_compat = true,
     config = {
         extra = {
-            xmult = 1.5
+            xmult = 2
         }
     },
 	loc_vars = function(self, info_queue, card)
